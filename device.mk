996ce01329aee7fbe1e5cec5c98df94585e9ce70
elektroschmock <elektroschmock78@googlemail.com>
2017-12-20 08:59:13 +0100

    shamu: Convert audio_effects.conf to XML
    * Based on https://android.googlesource.com/device/google/taimen/+/ecf82951a431324bd129b31495043dcfa00cfb1f
    
    Change-Id: I6bdbf127b0281ca0ad9401f208dad3a8ed73588f
    
    # Conflicts:
    #	device.mk

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    device/moto/shamu_t/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/moto/shamu_t/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     device/moto/shamu_t/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/moto/shamu_t/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     device/moto/shamu_t/audio/audio_policy_volumes_drc.xml:system/etc/audio_policy_volumes_drc.xml \
