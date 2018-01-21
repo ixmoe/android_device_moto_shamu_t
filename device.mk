643a1567db3529ca5c21f385367206f398284ed6
Elektroschmock <elektroschmock78@googlemail.com>
2018-01-21 12:12:24 +0100

    shamu: Move nfcee_access.xml into nfc folder
    Change-Id: I66e491f6250dc68516a67a7bb382d31dd6026e4b
    
    # Conflicts:
    #	device.mk

--- device.mk
+++ device.mk
@@ -316,12 +316,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
+    device/moto/shamu_t/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
     device/moto/shamu_t/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
     device/moto/shamu_t/nfc/libnfc-brcm-20795a10.conf:system/vendor/etc/libnfc-brcm-20795a10.conf
-
-# NFCEE access control
-PRODUCT_COPY_FILES += \
-    device/moto/shamu_t/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
