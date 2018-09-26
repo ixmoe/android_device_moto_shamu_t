LineageOS/android_device_moto_shamu

shamu: Reorganize and cleanup  BoardConfig
Change-Id: If4203b474b4040d796d9d5ee32caaa1f692f6e03

BoardConfig.mk
@@ -15,27 +15,92 @@
 # limitations under the License.
 #
 
+# CPU
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
-TARGET_NO_BOOTLOADER := true
-
-# Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-
+# Kernel
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M vmalloc=300M
+TARGET_KERNEL_CONFIG := shamu_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/shamu
 
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+# Platform
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := shamu
+TARGET_BOARD_PLATFORM := msm8084
+USE_CLANG_PLATFORM_BUILD := true
+
+# Audio
+BOARD_HAS_AUDIO_DSP := true
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+BOARD_USES_ALSA_AUDIO := true
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
+#Bluetooth defines
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/shamu/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/moto/shamu/bluetooth/vnd_shamu.txt
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA:= true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.shamu
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# Device manifest
+DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
+DEVICE_MATRIX_FILE := device/moto/shamu/compatibility_matrix.xml
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16793600
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+BOARD_OEMIMAGE_PARTITION_SIZE := 67108864
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 25253773312
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
+TARGET_HW_DISK_ENCRYPTION := false
+TARGET_USES_MKE2FS := true
+
+# GPS
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+TARGET_NO_RPC := true
+
+# ION
+TARGET_USES_ION := true
+
+# Power
+TARGET_HAS_LEGACY_POWER_STATS := true
+TARGET_HAS_NO_WIFI_STATS := true
+TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/1-004a/tsp"
+
+# Render
+HAVE_ADRENO_SOURCE:= false
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+USE_OPENGL_RENDERER := true
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -51,11 +116,18 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 # handled by the hardware composer
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 
-BOARD_USES_ALSA_AUDIO := true
+# Recovery
+LZMA_RAMDISK_TARGETS := recovery
+TARGET_RECOVERY_FSTAB = device/moto/shamu/rootdir/etc/fstab.shamu
 
-BOARD_SUPPORTS_SOUND_TRIGGER := true
+# RIL
+# Support Native Layer RF cutback
+BOARD_USES_CUTBACK_IN_RILD := true
 
-# Wifi related defines
+# Selinux
+BOARD_SEPOLICY_DIRS += device/moto/shamu/sepolicy
+
+# Wifi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WLAN_DEVICE           := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -66,79 +138,5 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_BUS := PCIE
-#BOARD_USES_SECURE_SERVICES := true
-
-#Bluetooth defines
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/shamu/bluetooth
-BOARD_CUSTOM_BT_CONFIG := device/moto/shamu/bluetooth/vnd_shamu.txt
-
-TARGET_NO_RADIOIMAGE := true
-TARGET_BOARD_PLATFORM := msm8084
-TARGET_BOOTLOADER_BOARD_NAME := shamu
-TARGET_NO_RPC := true
-
-TARGET_USES_ION := true
-TARGET_USES_64_BIT_BINDER := true
-TARGET_HW_DISK_ENCRYPTION := false
-
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16793600
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
-BOARD_OEMIMAGE_PARTITION_SIZE := 67108864
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 25253773312
-BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USES_MKE2FS := true
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.shamu
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
-
-# Power
-TARGET_HAS_LEGACY_POWER_STATS := true
-TARGET_HAS_NO_WIFI_STATS := true
-TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/1-004a/tsp"
-
-TARGET_RECOVERY_FSTAB = device/moto/shamu/rootdir/etc/fstab.shamu
-
-# Ensure f2fstools are built
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Support Native Layer RF cutback
-BOARD_USES_CUTBACK_IN_RILD := true
-
-BOARD_SEPOLICY_DIRS += device/moto/shamu/sepolicy
-
-HAVE_ADRENO_SOURCE:= false
-
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-
-BOARD_HAS_AUDIO_DSP := true
-
-USE_DEVICE_SPECIFIC_CAMERA:= true
-
-USE_CLANG_PLATFORM_BUILD := true
-
-TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
-
-# Render
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-
-# Recovery
-LZMA_RAMDISK_TARGETS := recovery
-
-# Device manifest
-DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
-DEVICE_MATRIX_FILE := device/moto/shamu/compatibility_matrix.xml
 
 -include vendor/motorola/shamu/BoardConfigVendor.mk

ixmoe/android_device_moto_shamu_t

BoardConfig.mk
@@ -16,27 +16,92 @@
 #
 TARGET_OTA_ASSERT_DEVICE := shamu,shamu_t,shamu_cn
 
+# CPU
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
-TARGET_NO_BOOTLOADER := true
-
-# Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_t_defconfig
-TARGET_KERNEL_SOURCE := kernel/moto/shamu_t
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-
+# Kernel
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=shamu ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M vmalloc=300M
+TARGET_KERNEL_CONFIG := shamu_t_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/shamu_t
 
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+# Platform
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := shamu_t
+TARGET_BOARD_PLATFORM := msm8084
+USE_CLANG_PLATFORM_BUILD := true
+
+# Audio
+BOARD_HAS_AUDIO_DSP := true
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+BOARD_USES_ALSA_AUDIO := true
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
+#Bluetooth defines
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/shamu_t/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/moto/shamu_t/bluetooth/vnd_shamu.txt
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA:= true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.shamu
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# Device manifest
+DEVICE_MANIFEST_FILE := device/moto/shamu_t/manifest.xml
+DEVICE_MATRIX_FILE := device/moto/shamu_t/compatibility_matrix.xml
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16793600
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1547483648
+BOARD_OEMIMAGE_PARTITION_SIZE := 67108864
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 25253773312
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_FS_CONFIG_GEN += device/moto/shamu_t/config.fs
+TARGET_HW_DISK_ENCRYPTION := false
+TARGET_USES_MKE2FS := true
+
+# GPS
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+TARGET_NO_RPC := true
+
+# ION
+TARGET_USES_ION := true
+
+# Power
+TARGET_HAS_LEGACY_POWER_STATS := true
+TARGET_HAS_NO_WIFI_STATS := true
+TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/1-004a/tsp"
+
+# Render
+HAVE_ADRENO_SOURCE:= false
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+USE_OPENGL_RENDERER := true
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -52,11 +117,18 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 # handled by the hardware composer
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 
-BOARD_USES_ALSA_AUDIO := true
+# Recovery
+LZMA_RAMDISK_TARGETS := recovery
+TARGET_RECOVERY_FSTAB = device/moto/shamu_t/rootdir/etc/fstab.shamu
 
-BOARD_SUPPORTS_SOUND_TRIGGER := true
+# RIL
+# Support Native Layer RF cutback
+BOARD_USES_CUTBACK_IN_RILD := true
 
-# Wifi related defines
+# Selinux
+BOARD_SEPOLICY_DIRS += device/moto/shamu_t/sepolicy
+
+# Wifi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WLAN_DEVICE           := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -67,79 +139,5 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_BUS := PCIE
-#BOARD_USES_SECURE_SERVICES := true
-
-#Bluetooth defines
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/shamu_t/bluetooth
-BOARD_CUSTOM_BT_CONFIG := device/moto/shamu_t/bluetooth/vnd_shamu.txt
-
-TARGET_NO_RADIOIMAGE := true
-TARGET_BOARD_PLATFORM := msm8084
-TARGET_BOOTLOADER_BOARD_NAME := shamu_t
-TARGET_NO_RPC := true
-
-TARGET_USES_ION := true
-TARGET_USES_64_BIT_BINDER := true
-TARGET_HW_DISK_ENCRYPTION := false
-
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16793600
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1547483648
-BOARD_OEMIMAGE_PARTITION_SIZE := 67108864
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 25253773312
-BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USES_MKE2FS := true
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.shamu
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
-
-# Power
-TARGET_HAS_LEGACY_POWER_STATS := true
-TARGET_HAS_NO_WIFI_STATS := true
-TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/1-004a/tsp"
-
-TARGET_RECOVERY_FSTAB = device/moto/shamu_t/rootdir/etc/fstab.shamu
-
-# Ensure f2fstools are built
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Support Native Layer RF cutback
-BOARD_USES_CUTBACK_IN_RILD := true
-
-BOARD_SEPOLICY_DIRS += device/moto/shamu_t/sepolicy
-
-HAVE_ADRENO_SOURCE:= false
-
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-
-BOARD_HAS_AUDIO_DSP := true
-
-USE_DEVICE_SPECIFIC_CAMERA:= true
-
-USE_CLANG_PLATFORM_BUILD := true
-
-TARGET_FS_CONFIG_GEN += device/moto/shamu_t/config.fs
-
-# Render
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-
-# Recovery
-LZMA_RAMDISK_TARGETS := recovery
-
-# Device manifest
-DEVICE_MANIFEST_FILE := device/moto/shamu_t/manifest.xml
-DEVICE_MATRIX_FILE := device/moto/shamu_t/compatibility_matrix.xml
 
 -include vendor/motorola/shamu_t/BoardConfigVendor.mk