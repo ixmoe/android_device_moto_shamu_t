LineageOS/android_device_moto_shamu

shamu: Accelerate boot by paralleling device enable
Some QCOM devices require sysfs to trigger boot/init which are blocking
the init process.
[    7.453205] init: Command 'write /sys/kernel/boot_adsp/boot 1' action=post-fs-data (/init.angler.rc:166) returned 0 took 271.936ms.
This CL is to put those slow to start devices in a
separate service and wait for the service to be done later on.

[Adapted for shamu: Elektroschmock]

Bug: 32712851
Test: On device
Change-Id: Idd4e965f122cbc8421b443a41573d363112dfa50

device.mk
@@ -26,7 +26,9 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.power.rc:root/init.shamu.power.rc \
     device/moto/shamu/init.shamu.usb.rc:root/init.shamu.usb.rc \
     device/moto/shamu/fstab.shamu:root/fstab.shamu \
-    device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc
+    device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc \
+    device/moto/shamu/rootdir/etc/init.qcom.devwait.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.devwait.sh \
+    device/moto/shamu/rootdir/etc/init.qcom.devstart.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.devstart.sh
 
 # Input device files for shamu
 PRODUCT_COPY_FILES += \

ixmoe/android_device_moto_shamu_t

device.mk
@@ -26,7 +26,9 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu_t/init.shamu.power.rc:root/init.shamu.power.rc \
     device/moto/shamu_t/init.shamu.usb.rc:root/init.shamu.usb.rc \
     device/moto/shamu_t/fstab.shamu:root/fstab.shamu \
-    device/moto/shamu_t/ueventd.shamu.rc:root/ueventd.shamu.rc
+    device/moto/shamu_t/ueventd.shamu.rc:root/ueventd.shamu.rc \
+    device/moto/shamu_t/rootdir/etc/init.qcom.devwait.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.devwait.sh \
+    device/moto/shamu_t/rootdir/etc/init.qcom.devstart.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.devstart.sh
 
 # Input device files for shamu
 PRODUCT_COPY_FILES += \